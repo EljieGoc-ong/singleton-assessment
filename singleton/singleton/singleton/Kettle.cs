a1acb2d5b1198b54a07d7ac4c7e9e391feeedc71
Eljie S. Goc-ong <tyrex092690@gmail.com>
2020-03-30 00:21:20 +0800

    singleton

---
+++ singleton/singleton/singleton/Kettle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Patterns.Singleton
+{
+    public class Kettle
+    {
+        private static readonly Kettle Instance = new Kettle();
+
+        public static Kettle ElectricKettleGet()
+        {
+            Console.WriteLine("Starting");
+            return Instance;
+        }
+
+        public void Fill()
+        {
+            Console.WriteLine("Filling...");
+        }
+
+        public void Drain()
+        {
+            Console.WriteLine("Draining...");
+        }
+
+        public void Boil()
+        {
+            Console.WriteLine("Boiling...");
+        }
+    }
+}
---
+++ singleton/singleton/singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Patterns.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kettle electricKettle = Kettle.ElectricKettleGet();
+
+            electricKettle.Fill();
+            electricKettle.Boil();
+            electricKettle.Drain();
+
+            Console.ReadLine();
+        }
+    }
+}
